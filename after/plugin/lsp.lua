a04726a3864654385ad248757bf94a5f817cfb66
lalosh <louayalosh17@gmail.com>
2023-01-29 12:55:57 +0200

    Fix: stop highlight for perf issues

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,18 +4,18 @@ local lspconfig = require("lspconfig")
 
 local function lsp_highlight_document(client)
   -- Set autocommands conditional on server_capabilities
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_exec(
-      [[
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]] ,
-      false
-    )
-  end
+  -- if client.server_capabilities.documentFormattingProvider then
+    -- vim.api.nvim_exec(
+      -- [[
+      -- augroup lsp_document_highlight
+        -- autocmd! * <buffer>
+        -- autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        -- autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      -- augroup END
+    -- ]] ,
+      -- false
+    -- )
+  -- end
 end
 
 local function lsp_keymaps(bufnr)
@@ -81,7 +81,7 @@ local function on_attach(client, bufnr)
 
   lsp_keymaps(bufnr)
 
-  lsp_highlight_document(client)
+  --lsp_highlight_document(client)
 
   setup()
 
--- lua/lalosh/plugins.lua
+++ lua/lalosh/plugins.lua
@@ -81,7 +81,6 @@ return packer.startup(function(use)
   
   -- cmp source
   use 'L3MON4D3/LuaSnip'
-  use "saadparwaiz1/cmp_luasnip"
   use "rafamadriz/friendly-snippets"
 
 
