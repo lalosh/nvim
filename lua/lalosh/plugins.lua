4e2b7d00af31a7146eb43f2100fc732ccc1b4dbd
lalosh <lalosh.dev@gmail.com>
2023-07-20 15:13:21 +0300

    add text objects for xml attributes

--- lua/lalosh/plugins.lua
+++ lua/lalosh/plugins.lua
@@ -98,6 +98,11 @@ return packer.startup(function(use)
   use 'kyazdani42/nvim-web-devicons'
   use 'kyazdani42/nvim-tree.lua'
 
+  -- add text object for xml attributes
+  use 'kana/vim-textobj-user'
+  use 'whatyouhide/vim-textobj-xmlattr'
+
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
