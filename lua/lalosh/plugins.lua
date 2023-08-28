1b88d1fc35a35ff44597f8b7d91aee93a508cf6f
lalosh <louay@pemo.io>
2023-08-28 14:45:55 +0300

    feat: add linter

--- lua/lalosh/plugins.lua
+++ lua/lalosh/plugins.lua
@@ -117,6 +117,8 @@ return packer.startup(function(use)
 
   use 'tree-sitter/tree-sitter-embedded-template'
 
+  use 'mfussenegger/nvim-lint'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
